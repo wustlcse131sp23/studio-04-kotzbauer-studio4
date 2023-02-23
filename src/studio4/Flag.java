2a6f4221e6c6c53b2bb54fed805c91078dccb17b
kevink <kevink@localhost>
2023-02-23 12:38:36 -0600

    V1

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,33 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenColor(255,20,255);
+		StdDraw.filledRectangle(0.5, 0.5, 0.45, 0.25);
+		
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.rectangle(0.5, 0.5, 0.45, 0.25);
+		StdDraw.setPenColor(155,155,155);
+		StdDraw.filledRectangle(0.75, 0.5, 0.05, 0.25);
+		StdDraw.setPenColor(155,155,155);
+		StdDraw.filledRectangle(0.25, 0.5, 0.05, 0.25);
+		StdDraw.setPenColor(255,255,255);
+		StdDraw.filledEllipse(0.5, 0.5, 0.3, 0.15);
+		StdDraw.setPenColor(0,0,0);
+		StdDraw.filledEllipse(0.5, 0.5, 0.2, 0.075);
+		StdDraw.setPenColor(255,0,0);
+		double[] x = { 0.43, 0.57, 0.5};
+		double[] y = { 0.43, 0.43, 0.57};
+		StdDraw.filledPolygon(x,y);
+
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -19,6 +20,32 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
-	}
+		String shape = in.next();
+		int[] color = new int[3];
+		for (int i =0; i<3; i++) {
+			color[i] = in.nextInt();
+		}
+		boolean fill = in.nextBoolean();
+		double[] dimensions = new double[4];
+		for (int i =0; i<4; i++) {
+			dimensions[i] = in.nextDouble();
+		}
+		StdDraw.setPenColor(color[0], color[1], color[2]);
+		if (shape.equals("rectangle")) {
+			if (fill == true) {
+				StdDraw.filledRectangle(dimensions[0], dimensions[1], dimensions[2], dimensions[3]);
+			}
+			else {
+				StdDraw.rectangle(dimensions[0], dimensions[1], dimensions[2], dimensions[3]);
+			}
+		}
+		if (shape.equals("ellipse")) {
+			if (fill == true) {
+				StdDraw.filledEllipse(dimensions[0], dimensions[1], dimensions[2], dimensions[3]);
+			}
+			else {
+				StdDraw.ellipse(dimensions[0], dimensions[1], dimensions[2], dimensions[3]);
+			}
+		}
+	}	
 }
